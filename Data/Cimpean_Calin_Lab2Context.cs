3ba0957a14632c609d86fe833c5b8a92d9fe1c56
calin <calin@DESKTOP-OKDA61E>
2022-10-30 21:40:30 +0200

    Add project files.

---
+++ Data/Cimpean_Calin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cimpean_Calin_Lab2.Models;
+
+namespace Cimpean_Calin_Lab2.Data
+{
+    public class Cimpean_Calin_Lab2Context : DbContext
+    {
+        public Cimpean_Calin_Lab2Context (DbContextOptions<Cimpean_Calin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cimpean_Calin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cimpean_Calin_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
